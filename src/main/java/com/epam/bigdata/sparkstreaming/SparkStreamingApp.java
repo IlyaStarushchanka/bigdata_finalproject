2187353e6d12087904c1cf844b76422b8ff23058
Ilya_Starushchanka <ilya10194@mail.ru>
2016-10-24 14:18:32 +0300

    spark streaming

---
+++ src/main/java/com/epam/bigdata/sparkstreaming/SparkStreamingApp.java
@@ -0,0 +1,76 @@
+package com.epam.bigdata.sparkstreaming;
+
+/**
+ * Created by Ilya_Starushchanka on 10/24/2016.
+ */
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.VoidFunction;
+import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.kafka.KafkaUtils;
+import org.elasticsearch.spark.rdd.api.java.JavaEsSpark;
+import org.spark_project.guava.collect.ImmutableList;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SparkStreamingApp {
+
+    private static final String SPLIT = "\\t";
+
+    public static void main(String[] args) throws Exception {
+
+        if (args.length == 0) {
+            System.err.println("Usage: SparkStreamingLogAggregationApp {zkQuorum} {group} {topic} {numThreads}");
+            System.exit(1);
+        }
+
+        String zkQuorum = args[0];
+        String group = args[1];
+        String[] topics = args[2].split(",");
+        int numThreads = Integer.parseInt(args[3]);
+
+        SparkConf sparkConf = new SparkConf().setAppName("SparkStreamingLogAggregationApp");
+        sparkConf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
+        sparkConf.set("es.index.auto.create", "true");
+
+        JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, new Duration(2000));
+
+        Map<String, Integer> topicMap = new HashMap<>();
+        for (String topic : topics) {
+            topicMap.put(topic, numThreads);
+        }
+
+        JavaPairReceiverInputDStream<String, String> messages = KafkaUtils.createStream(jssc, zkQuorum, group, topicMap);
+
+        JavaDStream<String> lines = messages.map(tuple2 -> {
+            String[] fields = tuple2._2().toString().split(SPLIT);
+            String json1 = "{\"type\" : \"logs\",\"ipinyour_id\" : \"" + fields[2] +"\"}";
+            System.out.println("####1");
+            System.out.println(json1);
+            return json1;
+        });
+
+        lines.foreachRDD(new VoidFunction<JavaRDD<String>>() {
+            @Override
+            public void call(JavaRDD<String> stringJavaRDD) throws Exception {
+                JavaEsSpark.saveJsonToEs(stringJavaRDD, "test/test");
+            }
+        });
+
+//        String json1 = "{\"reason\" : \"business\",\"airport\" : \"SFO\"}";
+//        String json2 = "{\"participants\" : 5,\"airport\" : \"OTP\"}";
+//
+//
+//        JavaRDD<String> stringRDD = jssc.parallelize(ImmutableList.of(json1, json2));
+//        JavaEsSpark.saveJsonToEs(stringRDD, "spark/json-trips");
+
+        lines.print();
+
+        jssc.start();
+        jssc.awaitTermination();
+    }
+}
