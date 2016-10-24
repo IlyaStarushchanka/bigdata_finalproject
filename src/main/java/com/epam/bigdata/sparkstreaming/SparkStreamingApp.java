3d8c5846b9eba1fd2e866ae09130de6a85652b53
Ilya_Starushchanka <ilya10194@mail.ru>
2016-10-24 16:13:10 +0300

    spark streaming

--- src/main/java/com/epam/bigdata/sparkstreaming/SparkStreamingApp.java
+++ src/main/java/com/epam/bigdata/sparkstreaming/SparkStreamingApp.java
@@ -45,21 +45,14 @@ public class SparkStreamingApp {
             topicMap.put(topic, numThreads);
         }
 
-        JSONObject jsonObject = new JSONObject("b382c1c156dcbbd5b9317cb50f6a747b    20130606000104000   Vh16OwT6OQNUXbj mozilla/4.0 (compatible; msie 6.0; windows nt 5.1; sv1; qqdownload 718) 180.127.189.*   80  87  1   tFKETuqyMo1mjMp45SqfNX  249b2c34247d400ef1cd3c6bfda4f12a        mm_11402872_1272384_3182279 300 250 1   1   0   00fccc64a1ee2809348509b7ac2a97a5    227 3427    282825712746    0");
-        String json1  =jsonObject.toString();
-        System.out.println("####1");
+        JavaPairReceiverInputDStream<String, String> messages = KafkaUtils.createStream(jssc, zkQuorum, group, topicMap);
 
-        System.out.println(json1);
-
-
-       JavaPairReceiverInputDStream<String, String> messages = KafkaUtils.createStream(jssc, zkQuorum, group, topicMap);
-
-       /* JavaDStream<String> lines = messages.map(tuple2 -> {
+        JavaDStream<String> lines = messages.map(tuple2 -> {
             LogsEntity logsEntity = new LogsEntity(tuple2._2().toString());
 
-            *//*String[] fields = tuple2._2().toString().split(SPLIT);
+            /*String[] fields = tuple2._2().toString().split(SPLIT);
 
-            String json1 = "{\"type\" : \"logs\",\"ipinyour_id\" : \"" + fields[2] +"\"}";*//*
+            String json1 = "{\"type\" : \"logs\",\"ipinyour_id\" : \"" + fields[2] +"\"}";*/
 
             JSONObject jsonObject = new JSONObject(logsEntity);
             String json1  =jsonObject.toString();
@@ -74,7 +67,7 @@ public class SparkStreamingApp {
             public void call(JavaRDD<String> stringJavaRDD) throws Exception {
                 JavaEsSpark.saveJsonToEs(stringJavaRDD, "test/test");
             }
-        });*/
+        });
 
 //        String json1 = "{\"reason\" : \"business\",\"airport\" : \"SFO\"}";
 //        String json2 = "{\"participants\" : 5,\"airport\" : \"OTP\"}";
@@ -83,8 +76,8 @@ public class SparkStreamingApp {
 //        JavaRDD<String> stringRDD = jssc.parallelize(ImmutableList.of(json1, json2));
 //        JavaEsSpark.saveJsonToEs(stringRDD, "spark/json-trips");
 
-        //lines.print();
-        messages.print();
+        lines.print();
+
         jssc.start();
         jssc.awaitTermination();
     }
